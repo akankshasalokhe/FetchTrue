570c74986388ff68624c8532dc4693d23d37e997
akankshasalokhe <akankshasalokhe2001@gmail.com>
2025-09-08 17:17:06 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "./globals.css";
-import './util/suppressHydrationWarnings'
+// import './util/suppressHydrationWarnings'
 
 export const metadata = {
   title: "Fetch True",
